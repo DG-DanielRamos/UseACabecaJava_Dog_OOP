1a9d21d64830662a286f0ce1f39593dc1f4dc6c5
Daniel Ramos <142344672+DG-DanielDev@users.noreply.github.com>
2024-06-21 12:05:02 -0300

    Add files via upload

---
+++ DogPoo/Dog.java
@@ -0,0 +1,14 @@
+class Dog {
+	int size;
+	String breed;
+	String name;
+
+	void bark() {
+		System.out.println("Ruff! Ruff!");
+	}
+	void dog() {
+		System.out.println("Nome: " + name);
+		System.out.println("Tamanho: " + size + " metros de altura");
+		System.out.println("Raça: " + breed);
+	}
+}
